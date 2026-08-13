478ec823cc06a51942989790d586d72296c0e68c
NRGTV <callumschmidt74@gmail.com>
2026-08-13 10:46:15 +1000

    feat: initialize gamepad + TV remote hooks at app boot

--- artifacts/energy-tv/src/main.tsx
+++ artifacts/energy-tv/src/main.tsx
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { initAdBlock } from "./lib/adblock";
+import useGamepad from "./hooks/useGamepad";
+import useTVRemote from "./hooks/useTVRemote";
 
 // Boot ad blocker BEFORE React renders (important for fetch/XHR patching)
 initAdBlock();
 
+function InputInitWrapper({ children }: { children: React.ReactNode }) {
+  // initialize global input hooks once
+  useGamepad();
+  useTVRemote();
+  return <>{children}</>;
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <InputInitWrapper>
+      <App />
+    </InputInitWrapper>
   </React.StrictMode>
 );
